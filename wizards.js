0990d7fbf3bd67cb36872161032ac9a8bfedb282
corbettlarsen <corbettlarsen@yahoo.com>
2018-05-05 22:56:59 -0600

    Progress on pathfinding DOES NOT RUN!

--- wizards.js
+++ wizards.js
@@ -1,4 +1,29 @@
 ROT.RNG.setSeed(1234);
+var map = {};
+var screen_width = 40;
+var screen_height = 40;
+var passableCallback = function(x, y) {
+    return (map[x+","+y] === 0);
+}
+var arena = new ROT.Map.Digger(screen_width,screen_height);
+var lightPasses = function(x, y) {
+    var key = x+","+y;
+    if (key in map) { return (map[key] == 0); }
+    return false;
+}
+var fov = new ROT.FOV.RecursiveShadowcasting(lightPasses);
+var callback = function(x,y,value){
+	map[x + "," + y] = value;
+}
+arena.create(callback);
+var character = new Entity(7,5,"@");
+var monster = new Entity(7,4,"!");
+place(character);
+place(monster);
+var display = new ROT.Display({width:screen_width, height:screen_height, forceSquareRatio:true});
+var debug = document.createElement("div");
+display.getContainer().addEventListener("click", getClickPosition);
+document.body.appendChild(display.getContainer());
 
 var getClickPosition = function(e) {
 	var square_width = display.getContainer().width/screen_width;
@@ -7,17 +32,22 @@ var getClickPosition = function(e) {
 	var click_y = (e.clientY + 6)/square_height;
 	var tile_x = Math.floor(click_x)-1;
 	var tile_y = Math.floor(click_y)-1;
-	//alert(tile_x + "," + tile_y);
 	display.draw(tile_x,tile_y,"C");
-}
+	var dijkstra = new ROT.Path.Dijkstra(character.x,character.y, passableCallback);
+	dijkstra.compute(tile_x, tile_y, function(x, y) {
+	display.draw(x, y, "", "", "#800");
+	character.patharray.push(x + ',' + y);
+   });
+	character.calcpath();
 
-var callback = function(x,y,value){
-	map[x + "," + y] = value;
 }
+display.getContainer().addEventListener("click", getClickPosition);
 
-function Entity(startX, startY){
+function Entity(startX, startY,icon){
   this.x = startX;
   this.y = startY;
+	this.patharray = [];
+	this.path = [];
   this.moveup = function(){
     if (!map[this.x+","+(this.y-1)]){
 		this.y -= 1;
@@ -38,6 +68,24 @@ function Entity(startX, startY){
 		this.x += 1;
 	}
   }
+	this.fovcomp = function(){
+		fov.compute(this.x, this.y, 10,function(x, y, r, visibility) {
+		var ch = (r ? " " : icon);
+		var color = (map[x+","+y] ? "#aa0": "#660");
+		display.draw(x, y, ch, "#fff", color);
+		 });
+	}
+	this.act = function(){
+    /* to do */
+	}
+	this.calcpath = function(){
+		alert(character.path);/*
+     for(var i = 0; i < this.patharray.length; i++){
+			 this.path.push(this.patharray[i+1][0]-this.patharray[i][0]);
+		 }
+		 */
+
+	}
 }
 
 function place(entity){
@@ -54,18 +102,6 @@ function place(entity){
 }
 
 var debug = document.createElement("div");
-
-ROT.RNG.setSeed(1234);
-var map = {};
-var screen_width = 40;
-var screen_height = 40;
-var arena = new ROT.Map.Digger(screen_width,screen_height);
-arena.create(callback);
-var character = new Entity(7,5);
-var monster = new Entity(7,4);
-place(character);
-place(monster);
-
 document.addEventListener("keydown", function(e) {
     var code = e.keyCode;
 
@@ -86,30 +122,14 @@ document.addEventListener("keydown", function(e) {
 	if (vk == "VK_LEFT"){
 		character.moveleft();
 	}
+	display.clear();
+	character.fovcomp();
 
-  var direction = Math.floor((Math.random() * 4) + 1);
-  if (direction == 1){
-  monster.moveup();
-}
-if (direction == 2){
-  monster.movedown();
-}
-if (direction == 3){
-  monster.moveright();
-}
-if (direction == 4){
-  monster.moveleft();
-}
-
-	for(i = 0;i <screen_width; i++){
-	for (j = 0; j < screen_height; j++){
-		display.draw(i,j,map[i + "," + j] ? "#" : " ");
-	}
-	display.draw(character.x,character.y,"@");
-  display.draw(monster.x,monster.y,"!");
-}
 });
-var display = new ROT.Display({width:screen_width, height:screen_height, forceSquareRatio:true});
-var debug = document.createElement("div");
-display.getContainer().addEventListener("click", getClickPosition);
-document.body.appendChild(display.getContainer());
+/*
+setInterval(function(){
+
+}, 100
+
+);
+*/
